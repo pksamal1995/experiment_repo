a75b33488ace4238ba97aa37221c6b92a30d7092
Pritam Kumar Samal <pritam.samal@s-force.org>
2020-02-21 00:25:43 +0530

    mistake rectified

--- src/test/java/com/experiment/jenkins/springjenkinstest/SpringJenkinsTestApplicationTests.java
+++ src/test/java/com/experiment/jenkins/springjenkinstest/SpringJenkinsTestApplicationTests.java
@@ -13,7 +13,7 @@ class SpringJenkinsTestApplicationTests {
 	private static Logger logger = LoggerFactory.getLogger(SpringJenkinsTestApplicationTests.class);
 	
 	@Test
-	 contextLoads() {
+	void contextLoads() {
 		logger.info("Test Case is executing.....");
 		assertEquals(true, true);
 	}
