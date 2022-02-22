af1d04aef1df7ec411f547704c6a8784a4879284
pritamsamal <pritamsamal@192.168.1.100>
2022-02-23 01:54:36 +0530

    test commit2 22feb22

--- src/main/java/com/experiment/jenkins/springjenkinstest/SpringJenkinsTestApplication.java
+++ src/main/java/com/experiment/jenkins/springjenkinstest/SpringJenkinsTestApplication.java
@@ -14,7 +14,7 @@ public class SpringJenkinsTestApplication {
 	
 	@PostConstruct
 	public void init() {
-		logger.info("Application Started.....>>>>>>>>>");
+		logger.info("Application Started.....>>>>");
 	}
 	public static void main(String[] args) {
 		logger.info("Application Executed....");
